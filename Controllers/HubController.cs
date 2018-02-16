f1ac67769db5f06e57de1015e5d7229d22d7b0f0
seed.of.apricot <seed.of.apricot@outlook.com>
2018-02-16 09:57:15 -0800

    Initial

---
+++ Controllers/HubController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CivLeagueJP.Web.Controllers
+{
+    public class SignalRHub : Hub
+    {
+        public Task Test1()
+        {
+            return Clients.AllExcept(new List<string> { "1" }).InvokeAsync("Test1");
+        }
+        public Task Test2()
+        {
+            return Clients.AllExcept(new List<string> { "2" }).InvokeAsync("Test2");
+        }
+    }
+}
